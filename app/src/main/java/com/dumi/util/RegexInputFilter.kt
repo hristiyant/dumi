e0d5204e7df0a82f02300c78bbbdc878adb355c0
Hristiyan.Todorov <hristiyan.todorov@adastragrp.com>
2019-10-07 15:17:46 +0300

    Update libs

--- app/src/main/java/com/dumi/util/RegexInputFilter.kt
+++ app/src/main/java/com/dumi/util/RegexInputFilter.kt
@@ -4,6 +4,12 @@ import android.text.InputFilter
 import android.text.Spanned
 import java.util.regex.Pattern
 
+/**
+ * This class is used to filter the user's input from the keyboard one symbol by symbol
+ * in order to prevent the user from entering anything other than the symbols allowed in the regex.
+ * Since the game is about entering words in a selected language, we only need to allow input of
+ * letters from the corresponding alphabet.
+ */
 const val CYRILLIC_CHARACTERS_ONLY_REGEX = "[А-ЯA-Z]"
 
 class RegexInputFilter : InputFilter {
@@ -17,10 +23,11 @@ class RegexInputFilter : InputFilter {
         dend: Int
     ): CharSequence? {
         val matcher = Pattern.compile(CYRILLIC_CHARACTERS_ONLY_REGEX).matcher(source)
+
         if (!matcher.matches()) {
             return ""
         }
-
+        // TODO: 7.10.2019 Try to adjust without nullable types
         return null
     }
 }
